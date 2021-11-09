6c84f39595ae61010ea655a017df9be3aa2f5f09
Fabian VC <fabianvera728@gmail.com>
2021-11-09 10:39:39 -0500

    [add] init navegation

--- app/src/main/java/com/sept/chuttypaws/MainActivity.kt
+++ app/src/main/java/com/sept/chuttypaws/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sept.chuttypaws
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+        // var imagePhoto: ImageView = findViewById(R.id.imagePhoto)
+        // var model = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        // Picasso.get().load(model.callUrlImage()).into(imagePhoto);
     }
 }
